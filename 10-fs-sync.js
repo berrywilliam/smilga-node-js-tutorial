fca96533668a8660216dce8b8cf538bfbb272448
william berry <wirrzbow@gmail.com>
2023-03-11 20:49:21 -0500

    worked on some more tutorial modules today

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -4,7 +4,8 @@ console.log('start');
 const first = readFileSync('./content/first.txt', 'utf8')
 const second = readFileSync('./content/second.txt', 'utf8')
 
-writeFileSync('./content/result-sync.txt', 
+writeFileSync(
+    './content/result-sync.txt', 
 `Here is the result : ${first}, ${second}`, 
 { flag: 'a'}
 )
